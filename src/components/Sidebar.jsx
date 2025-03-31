2e4eb4679576e337c4d5ca86eb10e62d6ee37917
grachevsergeye <sergey_grachev_03@mail.ru>
2025-03-31 23:37:11 +0300

    comleted

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,19 +16,19 @@ export default Sidebar;
 const DesktopSidebar = () => {
   return (
     <div className='p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block'>
-      <div className="flex flex-col gap-20 sticky top-10 left-0">
+      <div className="flex flex-col gap-10 sticky top-10 left-0">
         <div className="w-full">
-          <img src="/logo.svg" alt="logo" className='hidden md:block'/>
-          <img src="/mobile-logo.svg" alt="logo" className='block md:hidden'/>
+          <img src="/logo.svg" alt="logo" className='hidden md:block rounded-2xl'/>
+          <img src="/mobile-logo.png" alt="logo" className='block md:hidden rounded-2xl'/>
         </div>
         <ul className='flex flex-col items-center md:items-start gap-8'>
-          <Link to={"/"} className='flex gap-1'>
-            <Home size={24}/>
-            <span className="font-bold hidden md:block">Home</span>
+          <Link to={"/"} className='flex gap-2'>
+            <Home size={34}/>
+            <span className="font-bold text-2xl hidden md:block">Домой</span>
           </Link>
-          <Link to={"/favorites"} className='flex gap-1'>
-            <Heart size={24}/>
-            <span className="font-bold hidden md:block">Favorites</span>
+          <Link to={"/favorites"} className='flex gap-2'>
+            <Heart size={34}/>
+            <span className="font-bold text-2xl hidden md:block">Избранное</span>
           </Link>
         </ul>
       </div>
